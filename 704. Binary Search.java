04db4517a291a26bbd3bb7b2be8d57653be71de8
sparsh3896 <75719203+sparsh3896@users.noreply.github.com>
2021-01-06 22:44:08 +0530

    Create 704. Binary Search.java

---
+++ 704. Binary Search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) 
+    {
+      int high=nums.length-1,low=0;
+        
+        while(high>=low)
+        {
+            int mid= (high+low)/2;
+             if(nums[mid]==target)
+                 return mid;
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return -1;
+    }
+}
