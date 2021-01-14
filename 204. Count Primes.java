d82ab0729484903336993033b406012730ea43ad
sparsh3896 <75719203+sparsh3896@users.noreply.github.com>
2021-01-14 05:56:20 +0530

    Create 204. Count Primes.java

---
+++ 204. Count Primes.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        
+        return count;
+   }
+}
+//we use Seive of erastosthene to calculate the no of till n        
